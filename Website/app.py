8c4a8b786d3c35af0bd334443e6a57ad8b7dc3af
Miguel Wills <miguelcow1@gmail.com>
2025-04-23 13:52:38 -0600

    added crime and state

--- Website/app.py
+++ Website/app.py
@@ -40,9 +40,92 @@ def bird_states(bird_name):
     cur = conn.cursor()
     cur.execute("SELECT DISTINCT state FROM birds WHERE name = %s;", (bird_name,))
     states = [row[0] for row in cur.fetchall()]
+    placeholders = ','.join(f"'{state.upper()}'" for state in states) 
     cur.close()
     conn.close()
-    return jsonify(states=states)
+    
+    conn = get_db_connection()
+    curr2 = conn.cursor()
+    test = f'''
+WITH state_crime_totals AS (
+  SELECT state, category, SUM(value) AS total
+  FROM normalized_crime
+  WHERE state IN ({placeholders})
+    AND category NOT IN ('total_all_classes', 'other_offenses', 'other_assaults')
+  GROUP BY state, category
+),
+state_top_category AS (
+  SELECT DISTINCT ON (state) state, category
+  FROM state_crime_totals
+  ORDER BY state, total DESC
+),
+majority_top_category AS (
+  SELECT category, COUNT(*) AS freq
+  FROM state_top_category
+  GROUP BY category
+)
+SELECT category
+FROM majority_top_category
+ORDER BY freq DESC
+LIMIT 1;
+    ''', (placeholders,)
+    
+    top_crime = curr2.execute(f'''WITH state_crime_totals AS (
+  SELECT state, category, SUM(value) AS total
+  FROM normalized_crime
+  WHERE state IN ({placeholders})
+    AND category NOT IN ('total_all_classes', 'other_offenses', 'other_assaults')
+  GROUP BY state, category
+),
+state_top_category AS (
+  SELECT DISTINCT ON (state) state, category
+  FROM state_crime_totals
+  ORDER BY state, total DESC
+),
+majority_top_category AS (
+  SELECT category, COUNT(*) AS freq
+  FROM state_top_category
+  GROUP BY category
+)
+SELECT category
+FROM majority_top_category
+ORDER BY freq DESC
+LIMIT 1;
+''')
+    top_crime = curr2.execute("select * from birds limit 1;")
+    curr2.close()
+    conn.close()
+    print(test)
+    return jsonify(states=states,top_crime=top_crime)
+
+@app.route('/crime/by_bird/<bird_name>/<state_name>')
+def crime_states(bird_name,state_name):
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    # Now get the crime data for that state, ensuring the 'total_all_classes' category is excluded
+    cur.execute("""
+        SELECT category, value
+        FROM normalized_crime
+        WHERE state = (
+            SELECT distinct UPPER(state)
+            FROM birds
+            WHERE name = %s 
+            and state = %s
+        )
+        AND category != 'total_all_classes' and category != 'other_offenses' and category != 'other_assaults'
+        ORDER BY value DESC
+        LIMIT 1;
+    """, (bird_name,state_name,))
+
+    row = cur.fetchone()
+    cur.close()
+    conn.close()
+
+    if row:
+        return jsonify(category=row[0], max_value=row[1])
+    else:
+        return jsonify(error="No crime data found"), 404
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
